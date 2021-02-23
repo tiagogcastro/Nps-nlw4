22267eedb7f7e804bdeb4a01598b2ae5da01e353
Tiaguin061 <tiaguin180@gmail.com>
2021-02-23 12:03:36 -0300

    [feat] Day 2 finished, creating database, models and User controllers

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { Request, Response, NextFunction} from 'express';
+import { router } from './routes';
+
+import './database';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({
-    message: 'hello world'
-  });
-});
-
-app.post('/', (request, response) => {
-  return response.json({
-    message: 'Os dados foram salvos com sucesso'
-  });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running'));
